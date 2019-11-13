f0a07bf71f5307f0ab4b214014f81f36da7a1469
田邑隼一郎 <tamurahayabusaichirou@tamurahayabusaichirounoMacBook-puro.local>
2019-11-13 14:52:58 +0900

    [update] book::tamura

---
+++ app/controllers/book/tamura_controller.rb
@@ -0,0 +1,2 @@
+class Book::TamuraController < ApplicationController
+end
---
+++ test/controllers/book/tamura_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Book::TamuraControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
